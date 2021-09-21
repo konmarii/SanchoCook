203a60ac78674ff69d5e085f051ef8894b857fc9
konmarii <chankonworld@gmail.com>
2021-09-21 07:04:07 +0000

    [fix]activeadminの新規登録機能の削除

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,9 +2,9 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable, :registerable
+         :recoverable, :rememberable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable, :registerable
+         :recoverable, :rememberable, :validatable
 end

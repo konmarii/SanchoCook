e6e0554919bb6593cae719b2196ed6fa5a8e8a85
konmarii <chankonworld@gmail.com>
2021-09-24 23:38:32 +0000

    [add]配送ステータス

---
+++ db/migrate/20210924232205_add_delivery_status_to_orders.rb
@@ -0,0 +1,5 @@
+class AddDeliveryStatusToOrders < ActiveRecord::Migration[5.2]
+  def change
+    add_column :orders, :delivery_status, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_104538) do
+ActiveRecord::Schema.define(version: 2021_09_24_232205) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2021_09_23_104538) do
     t.integer "payment_method", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "delivery_status"
   end
 
   create_table "producers", force: :cascade do |t|

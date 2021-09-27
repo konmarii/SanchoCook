24a9aa6f71f4063f0bfa362dec20c36ceda5eb09
konmarii <chankonworld@gmail.com>
2021-09-27 14:42:50 +0000

    [fix]入金ステータスの条件分岐

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,4 +4,9 @@ class ContactMailer < ApplicationMailer
       @order = order
       mail to: customer.email, subject: 'ご注文確定のお知らせ'
     end
+    def bank_transfer_mail(customer, order)
+      @customer = customer
+      @order = order
+      mail to: customer.email, subject: '銀行振り込みのご案内'
+    end
 end

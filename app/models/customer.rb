ce3a378613570d5bd94c0b81e281031369789363
konmarii <chankonworld@gmail.com>
2021-09-28 05:39:21 +0000

    [add]新規登録のバリデーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,6 +17,14 @@ class Customer < ApplicationRecord
   has_many :active_customer_notifications, class_name: 'Notification', foreign_key: 'visitor_customer_id', dependent: :destroy
   has_many :passive_customer_notifications, class_name: 'Notification', foreign_key: 'visited_customer_id', dependent: :destroy
   
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
 
   def active_for_authentication?
     super && self.is_deleted == false

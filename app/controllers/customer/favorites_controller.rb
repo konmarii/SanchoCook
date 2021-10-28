92c1630e6c69bdabb3ab63a4d8062e2b77190004
konmarii <chankonworld@gmail.com>
2021-10-28 08:37:18 +0000

    [fix]favorites_controllerの修正

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -3,16 +3,23 @@ class Customer::FavoritesController < ApplicationController
 
   def index
     @favorite_products = FavoriteProduct.where(customer_id: current_customer.id)
+    
     @products = []
     @favorite_products.each do |favorite_product|
       @products << favorite_product.product
     end
-    @products.flatten!
+    
+    # @products = @favorite_products.each { |fp| fp.product }
+    
+
     @favorite_recipes = FavoriteRecipe.where(customer_id: current_customer.id)
+    
     @recipes = []
     @favorite_recipes.each do |favorite_recipe|
       @recipes << favorite_recipe.recipe
     end
-    @recipes.flatten!
+    
+    # @recipes = @favorite_recipes.each { |fr| fr.recipe }
+    
   end
 end

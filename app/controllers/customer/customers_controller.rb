a4f4d0a605db9644d71bebbe80c6031d21f2988c
konmarii <chankonworld@gmail.com>
2021-10-28 08:17:51 +0000

    [fix]customers_controllerの修正

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,13 +1,12 @@
 class Customer::CustomersController < ApplicationController
   before_action :authenticate_customer!
   before_action :correct_customer, only: [:edit, :unsubscribe]
+  before_action :set_customer
 
   def show
-    @customer = Customer.find(current_customer.id)
   end
 
   def edit
-    @customer = Customer.find(current_customer.id)
   end
 
   def update
@@ -17,24 +16,26 @@ class Customer::CustomersController < ApplicationController
   end
 
   def unsubscribe
-    @customer = Customer.find(current_customer.id)
   end
 
   def withdraw
-    @customer = Customer.find(current_customer.id)
     @customer.update(is_deleted: true)
     reset_session
     redirect_to root_path, success: "退会しました。ありがとうございました。"
   end
-  
+
+  private
   def correct_customer
     @customer = Customer.find(params[:id])
     unless @customer.id == current_customer.id
      redirect_to customer_path
     end
   end
-
-  private
+  
+  def set_customer
+    @customer = Customer.find(current_customer.id)
+  end
+  
   def customer_params
     params.require(:customer).permit(:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
   end

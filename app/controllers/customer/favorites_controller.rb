7d50436534ceee6feb3bbd50ff482b02d2d6d390
konmarii <chankonworld@gmail.com>
2021-10-29 02:23:47 +0000

    [fix]favorites_controllerのリファクタリング

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -4,22 +4,21 @@ class Customer::FavoritesController < ApplicationController
   def index
     @favorite_products = FavoriteProduct.where(customer_id: current_customer.id)
     
-    @products = []
-    @favorite_products.each do |favorite_product|
-      @products << favorite_product.product
-    end
-    
-    # @products = @favorite_products.each { |fp| fp.product }
-    
+    # @products = []
+    # @favorite_products.each do |favorite_product|
+    #   @products << favorite_product.product
+    # end
+    # ↓
+    @products = @favorite_products.map(&:product)
 
     @favorite_recipes = FavoriteRecipe.where(customer_id: current_customer.id)
     
-    @recipes = []
-    @favorite_recipes.each do |favorite_recipe|
-      @recipes << favorite_recipe.recipe
-    end
-    
-    # @recipes = @favorite_recipes.each { |fr| fr.recipe }
+    # @recipes = []
+    # @favorite_recipes.each do |favorite_recipe|
+    #   @recipes << favorite_recipe.recipe
+    # end
+    # ↓
+    @recipes = @favorite_recipes.map(&:recipe)
     
   end
 end
